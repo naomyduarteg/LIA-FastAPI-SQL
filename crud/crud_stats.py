da03c2f53587d9106aeea0c0b7566e657b2acf97
naomyduarteg <naomyduarteg@gmail.com>
2022-11-20 15:50:50 -0300

    more statistics added

--- crud/crud_stats.py
+++ crud/crud_stats.py
@@ -8,7 +8,8 @@ from sqlalchemy.sql import func
 
 
 def get_total_pages(db: Session, user_id: int):
-    total_pages = db.query(func.sum(models.Book.pages)).filter(models.Book.owner_id == user_id).scalar() #SELECT SUM(PAGES) FROM BOOKS
+    total_pages = db.query(func.sum(models.Book.pages)).filter(models.Book.owner_id == user_id).scalar()
+     #SELECT SUM(PAGES) FROM BOOKS
     if total_pages:
         return f"You have read {total_pages} pages so far! Keep on reading!"
 
@@ -16,19 +17,22 @@ def get_total_pages(db: Session, user_id: int):
 
 def get_mode_author(db: Session, user_id: int):
     mode_author = db.query(models.Book.author).filter(models.Book.owner_id == user_id).group_by(models.Book.author).order_by(func.count().desc()).limit(1).scalar()
-    #SELECT AUTHOR FROM BOOKS GROUP BY AUTHOR ORDER BY COUNT(*) DESC LIMIT 1
-    if mode_author:
-        return f"Your most read author is {mode_author}."
+     #SELECT AUTHOR FROM BOOK GROUP BY AUTHOR ORDER BY COUNT(*) DESC LIMIT 1
+    top_rated = db.query(models.Book.author).filter(models.Book.owner_id == user_id, models.Book.classification == 'Excellent').group_by(models.Book.author).order_by(func.count(models.Book.classification).desc()).limit(1).scalar()
+     #SELECT author FROM books WHERE classification = 'Excellent' GROUP BY author ORDER BY COUNT(classification) DESC LIMIT 1;
+    if mode_author and top_rated:
+        return f"Your most read and top rated author is, respectively, {mode_author} and {top_rated}."
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {user_id} not found!")
 
 def get_most_least_genre(db: Session, user_id: int, entry: str):
     if entry == 'most':
         mode_genre = db.query(models.Book.genre).filter(models.Book.owner_id == user_id).group_by(models.Book.genre).order_by(func.count().desc()).limit(1).scalar()
-        #SELECT AUTHOR FROM BOOKS GROUP BY AUTHOR ORDER BY COUNT(*) DESC LIMIT 1
+         #SELECT AUTHOR FROM BOOKS GROUP BY AUTHOR ORDER BY COUNT(*) DESC LIMIT 1
         return f"Your most read genre is {mode_genre}."
     if entry == 'least':
         least_genre = db.query(models.Book.genre).filter(models.Book.owner_id == user_id).group_by(models.Book.genre).order_by(func.count().asc()).limit(1).scalar()
+         #SELECT AUTHOR FROM BOOKS GROUP BY AUTHOR ORDER BY COUNT(*) ASC LIMIT 1
         return f"Your least read genre is {least_genre}."
     
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Entry not valid. Choose between 'most' or 'least'.")
--- endpoints/stats.py
+++ endpoints/stats.py
@@ -1,7 +1,6 @@
 from database import SessionLocal
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
-from schemas.books import Book, BookCreate, UpdateBook
 from crud import crud_stats
 from endpoints.users import get_db
 
@@ -13,17 +12,13 @@ def total_pages(user_id: int, db: Session = Depends(get_db)):
     total_pages = crud_stats.get_total_pages(db, user_id)
     return total_pages
 
-@router.get("/author/{most_read_author}", response_description="Most read author")
-def mode_author(user_id: int, db: Session = Depends(get_db)):
+@router.get("/author/{most_read_top_rated_author}", response_description="Most read and top rated author")
+def most_read_top_rated_author(user_id: int, db: Session = Depends(get_db)):
     mode_author = crud_stats.get_mode_author(db, user_id)
     return mode_author
 
-@router.get("/genre/{most_least_read_genre}", response_description="Most read genre")
+@router.get("/genre/{most_least_read_genre}", response_description="Most and least read genre")
 def genre(user_id: int, entry: str, db: Session = Depends(get_db)):
     genre = crud_stats.get_most_least_genre(db, user_id, entry)
     return genre
 
-# @router.get("/genre/{least_read_genre}", response_description="Least read genre")
-# def least_read_genre(user_id: int, db: Session = Depends(get_db)):
-#     mode_genre = crud_stats.get_least_read_genre(db, user_id)
-#     return mode_genre
